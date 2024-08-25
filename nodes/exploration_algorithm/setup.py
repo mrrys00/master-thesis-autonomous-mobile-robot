209a444bbc6fdf3d48110b376d18d2184b6e4e20
mrrys00 <rysszymon00@gmail.com>
2024-08-25 15:37:33 +0200

    feat: config for more nodes in module

--- nodes/exploration_algorithm/setup.py
+++ nodes/exploration_algorithm/setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages, setup
 
 package_name = 'exploration_algorithm'
+random_direction_node_name = 'random_direction_node'
+planned_node_name = 'planned_node'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     py_modules=[
-        f'{package_name}.{package_name}_node'
+        f'{package_name}.{random_direction_node_name}',
+        f'{package_name}.{planned_node_name}',
     ],
     install_requires=['setuptools'],
     data_files=[
@@ -17,13 +20,14 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            f'{package_name}_node = {package_name}.{package_name}_node:main'
+            f'{random_direction_node_name} = {package_name}.{random_direction_node_name}:main'
+            f'{planned_node_name} = {package_name}.{planned_node_name}:main'
         ],
     },
     zip_safe=True,
-    maintainer='szymon',
-    maintainer_email='szymon@todo.todo',
-    description='TODO: Package description',
+    maintainer='Szymon Rys',
+    maintainer_email='rysszymon00@gmail.com',
+    description='Exploration Algorithms',
     license='TODO: License declaration',
     tests_require=['pytest']
 )
