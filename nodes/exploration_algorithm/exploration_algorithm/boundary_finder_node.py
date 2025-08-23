37f9df905d84190fe5d0248079f899c6a5e95017
Szymon Ryś <rysszymon00@gmail.com>
2025-08-23 13:29:26 +0200

    fix: typing in boundary exploration

--- nodes/exploration_algorithm/exploration_algorithm/boundary_finder_node.py
+++ nodes/exploration_algorithm/exploration_algorithm/boundary_finder_node.py
@@ -1,12 +1,12 @@
 import rclpy
 from rclpy.node import Node
-from nav_msgs.msg import OccupancyGrid , Odometry
+from nav_msgs.msg import OccupancyGrid, Odometry
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 import numpy as np
-import heapq , math , random , yaml
+import heapq, math, random, yaml
 import scipy.interpolate as si
-import sys , threading , time
+import sys, threading, time
 
 
 with open("src/autonomous_exploration/config/params.yaml", 'r') as file:
@@ -20,7 +20,7 @@ robot_r = params["robot_r"]
 
 pathGlobal = 0
 
-def euler_from_quaternion(x,y,z,w):
+def euler_from_quaternion(x: float, y: float, z: float, w: float) -> float:
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
     roll_x = math.atan2(t0, t1)
@@ -31,13 +31,14 @@ def euler_from_quaternion(x,y,z,w):
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
     yaw_z = math.atan2(t3, t4)
+
     return yaw_z
 
-def heuristic(a, b):
+def heuristic(a: tuple[int], b: tuple[int]):
     return np.sqrt((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2)
 
 # -------------------- PATH PLANNING (A*) --------------------
-def astar(array, start, goal):
+def astar(array: list[float], start: tuple[int], goal: tuple[int]) -> list[tuple[int]] | bool:
     neighbors = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
     close_set = set()
     came_from = {}
@@ -74,27 +75,28 @@ def astar(array, start, goal):
                 gscore[neighbor] = tentative_g_score
                 fscore[neighbor] = tentative_g_score + heuristic(neighbor, goal)
                 heapq.heappush(oheap, (fscore[neighbor], neighbor))
-    # # UNCOMMENT If no path to goal was found, return closest path to goal
-    # if goal not in came_from:
-    #     closest_node = None
-    #     closest_dist = float('inf')
-    #     for node in close_set:
-    #         dist = heuristic(node, goal)
-    #         if dist < closest_dist:
-    #             closest_node = node
-    #             closest_dist = dist
-    #     if closest_node is not None:
-    #         data = []
-    #         while closest_node in came_from:
-    #             data.append(closest_node)
-    #             closest_node = came_from[closest_node]
-    #         data = data + [start]
-    #         data = data[::-1]
-    #         return data
+    # TO COMMENT If no path to goal was found, return closest path to goal
+    if goal not in came_from:
+        closest_node = None
+        closest_dist = float('inf')
+        for node in close_set:
+            dist = heuristic(node, goal)
+            if dist < closest_dist:
+                closest_node = node
+                closest_dist = dist
+        if closest_node is not None:
+            data = []
+            while closest_node in came_from:
+                data.append(closest_node)
+                closest_node = came_from[closest_node]
+            data = data + [start]
+            data = data[::-1]
+            return data
+
     return False
 
 # -------------------- B-SPLINE --------------------
-def bspline_planning(array, sn):
+def bspline_planning(array: list[tuple], sn: int) -> list[tuple]:
     try:
         array = np.array(array)
         x = array[:, 0]
@@ -121,7 +123,12 @@ def bspline_planning(array, sn):
     return path
 
 # -------------------- PURE PURSUIT --------------------
-def pure_pursuit(current_x, current_y, current_heading, path, index):
+def pure_pursuit(
+    current_x: float,
+    current_y: float,
+    current_heading: float,
+    path: list[tuple[float,float]],
+    index: int) -> tuple[float,float,int]:
     global lookahead_distance
     closest_point = None
     v = speed
@@ -148,11 +155,12 @@ def pure_pursuit(current_x, current_y, current_heading, path, index):
         sign = 1 if desired_steering_angle > 0 else -1
         desired_steering_angle = sign * math.pi/4
         v = 0.0
+
     return v,desired_steering_angle,index
 
 # -------------------- FRONTIER DETECTION --------------------
-def frontierB(matrix):
-    matrix_values = set(matrix.flatten())
+def frontierB(matrix: list[float]) -> list[float]:
+    # matrix_values = set(matrix.flatten())
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 0.0:
@@ -168,21 +176,28 @@ def frontierB(matrix):
                 if has_unknown and has_occupied:
                     matrix[i][j] = 2
                     print("[INFO] FRONTIER DETECTED AT", (i, j))
-    print(matrix)
-    print(matrix_values)
+    # print(matrix)
+    # print(matrix_values)
     return matrix
 
 # -------------------- GROUPING --------------------
-def assign_groups(matrix):
+def assign_groups(matrix: list[float]) -> tuple[list[float], dict[int, list[tuple[int, int]]]]:
     group = 1
     groups = {}
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] == 2:
                 group = dfs(matrix, i, j, group, groups)
+
     return matrix, groups
 
-def dfs(matrix, i, j, group, groups):
+def dfs(
+        matrix: list[float],
+        i: int,
+        j: int,
+        group: int,
+        groups: dict[int, list[tuple[int, int]]]
+    ) -> int:
     if i < 0 or i >= len(matrix) or j < 0 or j >= len(matrix[0]):
         return group
     if matrix[i][j] != 2:
@@ -191,6 +206,7 @@ def dfs(matrix, i, j, group, groups):
         groups[group].append((i, j))
     else:
         groups[group] = [(i, j)]
+
     matrix[i][j] = 0
     dfs(matrix, i + 1, j, group, groups)
     dfs(matrix, i - 1, j, group, groups)
@@ -200,11 +216,19 @@ def dfs(matrix, i, j, group, groups):
     dfs(matrix, i - 1, j - 1, group, groups)
     dfs(matrix, i - 1, j + 1, group, groups)
     dfs(matrix, i + 1, j - 1, group, groups)
+
     return group + 1
 
 
 # -------------------- OBSTACLE CLUSTERING --------------------
-def dfs_obstacle(matrix, i, j, group, groups):
+# TODO: merge with frontier grouping
+def dfs_obstacle(
+        matrix: list[float],
+        i: int,
+        j: int,
+        group: int,
+        groups: dict[int, list[tuple[int, int]]]
+    ) -> int:
     if i < 0 or i >= len(matrix) or j < 0 or j >= len(matrix[0]):
         return group
     if matrix[i][j] != 100:
@@ -213,6 +237,7 @@ def dfs_obstacle(matrix, i, j, group, groups):
         groups[group].append((i, j))
     else:
         groups[group] = [(i, j)]
+
     matrix[i][j] = -999  # mark visited
     dfs_obstacle(matrix, i + 1, j, group, groups)
     dfs_obstacle(matrix, i - 1, j, group, groups)
@@ -222,23 +247,25 @@ def dfs_obstacle(matrix, i, j, group, groups):
     dfs_obstacle(matrix, i - 1, j - 1, group, groups)
     dfs_obstacle(matrix, i - 1, j + 1, group, groups)
     dfs_obstacle(matrix, i + 1, j - 1, group, groups)
+
     return group + 1
 
-def assign_obstacle_groups(matrix):
+def assign_obstacle_groups(matrix: list[float]) -> dict[int, list[tuple[int, int]]]:
     group = 1
     groups = {}
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] == 100:
                 group = dfs_obstacle(matrix, i, j, group, groups)
+
     return groups
 
 # -------------------- FRONTIER SELECTION --------------------
-def fGroups(groups):
+def fGroups(groups: dict[int, list[tuple[int, int]]]) -> list[tuple[int, list[tuple[int, int]]]]:
     sorted_groups = sorted(groups.items(), key=lambda x: len(x[1]), reverse=True)
     return sorted_groups
 
-def calculate_centroid(x_coords, y_coords):
+def calculate_centroid(x_coords: list[int], y_coords: list[int]) -> tuple[int, int]:
     n = len(x_coords)
     return (int(sum(x_coords) / n), int(sum(y_coords) / n))
 
@@ -250,7 +277,14 @@ def frontierTouchesObstacle(frontier_group, obstacle_group):
                 return True
     return False
 
-def findClosestGroup(matrix, groups, current, resolution, originX, originY):
+def findClosestGroup(
+    matrix: list[float],
+    groups: list[tuple[int, list[tuple[int, int]]]],
+    current: tuple[int],
+    resolution: float,
+    originX: float,
+    originY: float) -> list[tuple[float,float]] | None:
+    
     targetP = None
     obstacle_groups = assign_obstacle_groups(matrix.copy())
     largest_obstacle = max(obstacle_groups.items(), key=lambda x: len(x[1]))[1] if obstacle_groups else []
@@ -259,16 +293,22 @@ def findClosestGroup(matrix, groups, current, resolution, originX, originY):
     for gid, frontier_group in groups:
         if frontierTouchesObstacle(frontier_group, largest_obstacle):
             chosen_group = frontier_group
+            print("[INFO] CHOSEN GROUP TOUCHES OBSTACLE")
             break
 
     if not chosen_group and groups:
+        print("[INFO] NO GROUP TOUCHES OBSTACLE, CHOOSING LARGEST GROUP")
         chosen_group = groups[0][1]  # fallback: biggest frontier
 
     if chosen_group:
-        middle = calculate_centroid([p[0] for p in chosen_group],[p[1] for p in chosen_group]) 
+        middle = calculate_centroid([p[0] for p in chosen_group], [p[1] for p in chosen_group]) 
         path = astar(matrix, current, middle)
+        # if not path:  # If uncommented finishes the exploration too early
+        #     print("[INFO] NO PATH FOUND")
+        #     return None
         path = [(p[1]*resolution+originX,p[0]*resolution+originY) for p in path]
         targetP = path
+
     return targetP
 
 # -------------------- COSTMAP + EXPLORATION --------------------
@@ -278,9 +318,14 @@ def pathLength(path):
     distances = np.hypot(differences[:,0], differences[:,1])
     return np.sum(distances)
 
-def costmap(data,width,height,resolution):
-    data = np.array(data).reshape(height,width)
-    wall = np.where(data == 100)
+def costmap(
+    map_occupancy_data: list[int],
+    width: int,
+    height: int,
+    resolution: float) -> list[float]:
+    
+    map_occupancy_data = np.array(map_occupancy_data).reshape(height,width)
+    wall = np.where(map_occupancy_data == 100)
     for i in range(-expansion_size,expansion_size+1):
         for j in range(-expansion_size,expansion_size+1):
             if i  == 0 and j == 0:
@@ -289,32 +334,55 @@ def costmap(data,width,height,resolution):
             y = wall[1]+j
             x = np.clip(x,0,height-1)
             y = np.clip(y,0,width-1)
-            data[x,y] = 100
-    data = data*resolution
-    return data
+            map_occupancy_data[x,y] = 100
+    map_occupancy_data = map_occupancy_data*resolution
 
-def exploration(data,width,height,resolution,column,row,originX,originY):
-        global pathGlobal
-        data = costmap(data,width,height,resolution)
-        data[row][column] = 0
-        data[data > 5] = 1
-        data = frontierB(data)
-        data,groups = assign_groups(data)
-        groups = fGroups(groups)
-        if len(groups) == 0:
-            path = -1
+    return map_occupancy_data
+
+def exploration(
+    map_occupancy_data: list[int] | list[float],
+    width: int,
+    height: int,
+    resolution: float,
+    column: int,
+    row: int,
+    originX: float,
+    originY: float):
+
+    global pathGlobal
+    map_occupancy_data = costmap(
+        map_occupancy_data,
+        width,
+        height,
+        resolution)
+    map_occupancy_data[row][column] = 0
+    map_occupancy_data[map_occupancy_data > 5] = 1
+    map_occupancy_data = frontierB(map_occupancy_data)
+    map_occupancy_data, groups = assign_groups(map_occupancy_data)
+    
+    groups = fGroups(groups)
+    
+    if len(groups) == 0:
+        path = -1
+    else:
+        map_occupancy_data[map_occupancy_data < 0] = 1
+        path = findClosestGroup(
+            map_occupancy_data,
+            groups,
+            (row,column),
+            resolution,
+            originX,
+            originY)
+        if path != None:
+            path = bspline_planning(path, len(path)*5)
         else:
-            data[data < 0] = 1
-            path = findClosestGroup(data,groups,(row,column),resolution,originX,originY)
-            if path != None:
-                path = bspline_planning(path,len(path)*5)
-            else:
-                path = -1
-        pathGlobal = path
-        return
+            path = -1
+    pathGlobal = path
+    
+    return
 
 # -------------------- LOCAL CONTROL --------------------
-def localControl(scan):
+def localControl(scan) -> tuple[float,float | None,None]:
     v = None
     w = None
     for i in range(60):
@@ -334,13 +402,37 @@ def localControl(scan):
 class navigationControl(Node):
     def __init__(self):
         super().__init__('Exploration')
-        self.subscription = self.create_subscription(OccupancyGrid,'map',self.map_callback,10)
-        self.subscription = self.create_subscription(Odometry,'odom',self.odom_callback,10)
-        self.subscription = self.create_subscription(LaserScan,'scan',self.scan_callback,10)
+        self.subscription = self.create_subscription(OccupancyGrid, 'map', self.map_callback, 10)
+        self.subscription = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
+        self.subscription = self.create_subscription(LaserScan, 'scan', self.scan_callback, 10)
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         print("[INFO] EXPLORATION MODE ACTIVE")
-        self.kesif = True
+        self.target = True
         threading.Thread(target=self.exp).start()
+        
+        self.scan_data: LaserScan | None
+        self.scan: list[float] | any
+        
+        self.map_data: OccupancyGrid | None
+        self.resolution: float | any
+        self.originX: float | any
+        self.originY: float | any
+        self.width: int | any
+        self.height: int | any
+        self.map_occupancy_data: list[int] | any
+        
+        self.odom_data: Odometry | None
+        self.x: float | any
+        self.y: float | any
+        self.yaw: float | any
+        
+        self.path: list[tuple[any | list[list | any], any | list[list | any]]]  |  np.ndarray[tuple[int, ...], np.dtype]  |  int
+        
+        self.target: bool
+        self.i: int
+        self.c: int
+        self.r: int
+        self.t: threading.Timer
         
     def exp(self):
         twist = Twist()
@@ -348,11 +440,18 @@ class navigationControl(Node):
             if not hasattr(self,'map_data') or not hasattr(self,'odom_data') or not hasattr(self,'scan_data'):
                 time.sleep(0.1)
                 continue
-            if self.kesif == True:
+            if self.target == True:
                 if isinstance(pathGlobal, int) and pathGlobal == 0:
                     column = int((self.x - self.originX)/self.resolution)
                     row = int((self.y- self.originY)/self.resolution)
-                    exploration(self.data,self.width,self.height,self.resolution,column,row,self.originX,self.originY)
+                    exploration(self.map_occupancy_data, 
+                        self.width, 
+                        self.height, 
+                        self.resolution, 
+                        column, 
+                        row, 
+                        self.originX, 
+                        self.originY)
                     self.path = pathGlobal
                 else:
                     self.path = pathGlobal
@@ -361,21 +460,26 @@ class navigationControl(Node):
                     sys.exit()
                 self.c = int((self.path[-1][0] - self.originX)/self.resolution) 
                 self.r = int((self.path[-1][1] - self.originY)/self.resolution) 
-                self.kesif = False
+                self.target = False
                 self.i = 0
                 print("[INFO] NEW GOAL SELECTED")
                 t = pathLength(self.path)/speed
                 t = t - 0.2
-                self.t = threading.Timer(t,self.target_callback)
+                self.t = threading.Timer(t, self.target_callback)
                 self.t.start()
             else:
                 v , w = localControl(self.scan)
                 if v == None:
-                    v, w,self.i = pure_pursuit(self.x,self.y,self.yaw,self.path,self.i)
+                    v, w, self.i = pure_pursuit(
+                        self.x,
+                        self.y,
+                        self.yaw,
+                        self.path,
+                        self.i)
                 if(abs(self.x - self.path[-1][0]) < target_error and abs(self.y - self.path[-1][1]) < target_error):
                     v = 0.0
                     w = 0.0
-                    self.kesif = True
+                    self.target = True
                     print("[INFO] GOAL REACHED")
                     self.t.join()
                 twist.linear.x = v
@@ -384,27 +488,38 @@ class navigationControl(Node):
                 time.sleep(0.1)
 
     def target_callback(self):
-        exploration(self.data,self.width,self.height,self.resolution,self.c,self.r,self.originX,self.originY)
+        exploration(
+            self.map_occupancy_data,
+            self.width,
+            self.height,
+            self.resolution,
+            self.c,
+            self.r,
+            self.originX,
+            self.originY)
         
-    def scan_callback(self,msg):
+    def scan_callback(self, msg: LaserScan):
         self.scan_data = msg
         self.scan = msg.ranges
 
-    def map_callback(self,msg):
+    def map_callback(self, msg: OccupancyGrid):
         self.map_data = msg
         self.resolution = self.map_data.info.resolution
         self.originX = self.map_data.info.origin.position.x
         self.originY = self.map_data.info.origin.position.y
         self.width = self.map_data.info.width
         self.height = self.map_data.info.height
-        self.data = self.map_data.data
+        self.map_occupancy_data = self.map_data.data
 
-    def odom_callback(self,msg):
+    def odom_callback(self, msg: Odometry):
         self.odom_data = msg
         self.x = msg.pose.pose.position.x
         self.y = msg.pose.pose.position.y
-        self.yaw = euler_from_quaternion(msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,
-        msg.pose.pose.orientation.z,msg.pose.pose.orientation.w)
+        self.yaw = euler_from_quaternion(
+            msg.pose.pose.orientation.x,
+            msg.pose.pose.orientation.y, 
+            msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w)
 
 
 def main(args=None):
